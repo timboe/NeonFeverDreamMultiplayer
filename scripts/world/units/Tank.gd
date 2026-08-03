f6d546a6ad2f21489f35fcae84a096d6fdd11107
Tim Martin <timboe@gmail.com>
2026-08-03 12:21:28 +0100

    fix beam color and var shadow

--- scripts/world/units/Tank.gd
+++ scripts/world/units/Tank.gd
@@ -18,11 +18,9 @@ func initialise(b: Building) -> void:
 	_beam_node.mesh = _make_beam_mesh()
 	var mat := StandardMaterial3D.new()
 	mat.emission_enabled = true
-	#mat.emission = Color(1.0, 0.2, 0.05)
-	mat.emission = Config.PLAYER_COLORS[ player_owner ]
+	mat.emission = Config.PLAYER_COLORS[ player_owner - 1 ]
 	mat.emission_energy_multiplier = 5.0
-	#mat.albedo_color = Color(1.0, 0.15, 0.05, 0.85)
-	mat.albedo_color = Config.PLAYER_COLORS[ player_owner ]
+	mat.albedo_color = Config.PLAYER_COLORS[ player_owner - 1 ]
 	mat.albedo_color.a = 0.85
 	mat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
 	_beam_node.material_override = mat
@@ -80,10 +78,10 @@ func _update_combat_visuals(delta: float) -> void:
 			x = Vector3.RIGHT
 		x = x.normalized()
 		var z = x.cross(y).normalized()
-		var basis = Basis(x, y, z)
-		basis.x *= 0.35
-		basis.y *= dist
-		basis.z *= 0.35
-		_beam_node.global_transform = Transform3D(basis, from)
+		var new_basis = Basis(x, y, z)
+		new_basis.x *= 0.35
+		new_basis.y *= dist
+		new_basis.z *= 0.35
+		_beam_node.global_transform = Transform3D(new_basis, from)
 	elif _beam_node:
 		_beam_node.visible = false
